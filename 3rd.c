9118f8b27fdb2eddfec8a1bcded73f2564fb7907
sameer169 <35867492+sameer169@users.noreply.github.com>
2019-04-18 21:17:34 +0530

    Create 3rd.c

---
+++ 3rd.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+int main()
+{
+int i,n,pr[10]={1,2,3,4,5,6,7,8,9,10},min,k=1,btime=0;
+int bur[10],temp,j,arr[10],wait[10],tat[10],ta=0,sum=0;
+float wavg=0,tavg=0,tsum=0,wsum=0;
+printf(" -------Shortest Job First Scheduling ( NP )-------\n");
+printf("\nEnter the No. of processes :");
+scanf("%d",&n);
+ 
+for(i=0;i<n;i++)
+{
+printf("\tEnter the burst time of %d process :",i+1);
+scanf(" %d",&bur[i]);
+
+}
+for(i=0;i<n;i++)
+{
+printf("\tEnter the arrival time of %d process :",i+1);
+scanf(" %d",&arr[i]);
+}
+ 
+/*Sorting According to Arrival Time*/
+ 
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+if(at[i]<at[j])
+{
+temp=pr[j];
+pr[j]=pr[i];
+pr[i]=temp;
+temp=arr[j];
+arr[j]=arr[i];
+arr[i]=temp;
+temp=bur[j];
+bur[j]=bur[i];
+bur[i]=temp;
+}
+}
+}
+ 
+
+ 
+for(j=0;j<n;j++)
+{
+btime=btime+bt[j];
+min=bur[k];
+for(i=k;i<n;i++)
+{
+if (btime>=arr[i] && bur[i]<min)
+{
+temp=pr[k];
+pr[k]=pr[i];
+pr[i]=temp;
+temp=arr[k];
+arr[k]=arr[i];
+arr[i]=temp;
+temp=bur[k];
+bur[k]=bur[i];
+bur[i]=temp;
+}
+}
+k++;
+}
+wait[0]=0;
+for(i=1;i<n;i++)
+{
+sum=sum+bur[i-1];
+wait[i]=sum-arr[i];
+wsum=wsum+wait[i];
+}
+ 
+wavg=(wsum/n);
+for(i=0;i<n;i++)
+{
+ta=ta+bur[i];
+tat[i]=ta-arr[i];
+tsum=tsum+tat[i];
+}
+ 
+tavg=(tsum/n);
+ 
+printf("************************");
+printf("\n RESULT:-");
+printf("\nProcess\t Burst\t Arrival\t Waiting\t Turn-around" );
+for(i=0;i<n;i++)
+{
+printf("\n p%d\t %d\t %d\t\t %d\t\t\t%d",pr[i],bur[i],arr[i],wait[i],tat[i]);
+}
+ 
+printf("\n\nAVERAGE WAITING TIME : %f",wavg);
+printf("\nAVERAGE TURN AROUND TIME : %f",tavg);
+return 0;
+}
